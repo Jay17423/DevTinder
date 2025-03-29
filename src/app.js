e65b6835b66d3c16eb7264875ba36e538c6130d6
Jay Prakash <jayprakash1200@gmail.com>
2025-03-29 11:20:13 +0530

    Exploring GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send("FirstName: Jay, LastName: Prakash");
+})
+
+app.post("/user", (req, res) => {
+  // console.log("save data to database");
+  res.send("Data saved to database");
+})
+
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
 });
