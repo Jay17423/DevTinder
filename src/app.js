5eb2b7a6a5bd2fbdcd057d99b28c2b351b645d6e
Jay Prakash <jayprakash1200@gmail.com>
2025-03-30 12:16:23 +0530

    Learn about the error handling

--- src/app.js
+++ src/app.js
@@ -2,30 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use("/admin",(req,res,next)=>{
-  console.log("Admin auth is getting Checked");
-  
-  const token = "xyzjgdfgdf";
-  const isAdminfAuthorized =  token === "xyzjgdfgdf";
-  if(!isAdminfAuthorized){
-    return res.status(401).send("Unauthorized");
+
+app.use("/getUserData",(req,res) =>{
+  try {
+    throw new Error("Something went wrong");
+    res.send("User Data Sent");
+  } catch (error) {
+    res.status(500).send("Some internal error has been occured contact Team!"); 
   }
-  else{
-    next();
-  }
-});
-
-app.get("/user",(req,res)=>{
-  res.send("User Data");
 })
 
-app.get("/admin/getAllData",(req,res)=>{
-  res.send("Admin Data"); 
-})
 
-app.get("/admin/deleteUser",(req,res)=>{
-  res.send("User Deleted");
-})
 
 
 app.listen(3000, () => console.log("Server  running on port 3000"));
