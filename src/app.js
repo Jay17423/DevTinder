652370d689999be507e051387ae645adb449cdd0
Jay Prakash <jayprakash1200@gmail.com>
2025-03-30 11:43:13 +0530

    Learn about the middleWares

--- src/app.js
+++ src/app.js
@@ -2,21 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res,next) => {
-    next();
-    res.send("Response from first middleware");
-  },
-  (req, res) => {
-    res.send("Response from second middleware");
-  },
-  (req, res) => {
-    res.send("Response from Third middleware");
-  },
-  (req, res) => {
-    res.send("Response from fourth middleware");
+app.use("/admin",(req,res,next)=>{
+  console.log("Admin auth is getting Checked");
+  
+  const token = "xyzjgdfgdf";
+  const isAdminfAuthorized =  token === "xyzjgdfgdf";
+  if(!isAdminfAuthorized){
+    return res.status(401).send("Unauthorized");
   }
-);
+  else{
+    next();
+  }
+});
+
+app.get("/user",(req,res)=>{
+  res.send("User Data");
+})
+
+app.get("/admin/getAllData",(req,res)=>{
+  res.send("Admin Data"); 
+})
+
+app.get("/admin/deleteUser",(req,res)=>{
+  res.send("User Deleted");
+})
+
 
 app.listen(3000, () => console.log("Server  running on port 3000"));
