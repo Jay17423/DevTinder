b61ea6a75a1dedaa7ad8d20e6517b47382bdd36e
Jay Prakash <jayprakash1200@gmail.com>
2025-03-31 12:04:53 +0530

    Created a /signup api to push data int o database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user.js");
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Virat",
+    lastName: "Kohli",
+    email: "4k2wV@example.com",
+    password: "123456",
+    age: 21,
+    gender: "male",
+  }
+
+  //Creating a new instance of the user model
+  const user = new User(userObj);
+  await user.save();
+  res.send("User created successfully");
+})
 
 connectDB()
   .then(() => {
