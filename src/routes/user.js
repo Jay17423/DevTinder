1a0875348228d4cc8d6f23a5aa065da87365a895
Jay17423 <jayprakash1200@gmail.com>
2025-04-15 20:24:29 +0530

    Done today work

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName email photoUrl about skills age";
+
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -24,11 +26,19 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       $or: [
-        { fromUserId: loggedInUser._id, status: "accepted" },
         { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", "firstName lastName email photoUrl about skills age");
-    const data = connectionRequests.map((row) => row.fromUserId);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
     res.json({
       message: "Connections fetched successfully",
       data,
