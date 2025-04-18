dbf6141b22491185a8188b8a4eb6f79b7b54460f
Jay17423 <jayprakash1200@gmail.com>
2025-04-18 11:39:13 +0530

    Created the footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-    <Navbar />
-      
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
