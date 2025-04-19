6fe5baa57ecba9ef23df2fbf52bb6a3cf7fbbba2
Jay17423 <jayprakash1200@gmail.com>
2025-04-19 17:11:23 +0530

    Setup feedStore and feed api call

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
 const Feed = () => {
+  const dispatch = useDispatch;
+  const feed = useSelector((store) => store.feed);
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error("Error fetching feed data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
   return (
     <div>
-      <p className='text-3xl'>Feed</p>
+      <p className="text-3xl">Feed</p>
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
